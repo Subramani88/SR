eae9bd841c8f1f2163dfc2e06ddc958877396c04
krahamath <rahamathullah.mk@gmail.com>
2019-10-07 10:51:08 +0200

    Removing unwated and ENV

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 define('APP_PATH', '/');
 require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
 
 // Initiate
 $request = \Klein\Request::createFromGlobals();
